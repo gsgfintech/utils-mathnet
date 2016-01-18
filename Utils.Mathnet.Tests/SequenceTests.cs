15b36e460ea69ca8e94b6081cbdfcc42b89fd62c
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2016-01-18 09:45:13 +0800

    Add sequence methods IsMonotonic and IsStrictlyMonotonic

---
+++ Utils.Mathnet.Tests/SequenceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.Teirlinck.Utils.Mathnet;
+
+namespace Utils.Mathnet.Tests
+{
+    [TestClass]
+    public class SequenceTests
+    {
+        [TestMethod]
+        public void TestIsMonotonic()
+        {
+            Assert.IsTrue((new double[] { 0, 0, 0, 0 }).IsMonotonic());
+            Assert.IsTrue((new int[] { -1, 0, 0, 3, 4, 4, 5 }).IsMonotonic());
+            Assert.IsFalse((new int[] { -1, 0, 0, 3, 2, 4, 5 }).IsMonotonic());
+            Assert.IsTrue((new double[] { -1.5, 0, 0.5, 3, 4, 4.2, 5 }).IsMonotonic());
+            Assert.IsTrue((new double[] { 5, 4.2, 4, 3, 0.5, 0, -1.5 }).IsMonotonic());
+            Assert.IsTrue((new double[] { 5, 4, 4, 3, 0.5, 0, -1.5 }).IsMonotonic());
+        }
+
+        [TestMethod]
+        public void TestIsStrictlyMonotonic()
+        {
+            Assert.IsFalse((new double[] { 0, 0, 0, 0 }).IsStrictlyMonotonic());
+            Assert.IsFalse((new int[] { -1, 0, 0, 3, 4, 4, 5 }).IsStrictlyMonotonic());
+            Assert.IsFalse((new int[] { -1, 0, 0, 3, 2, 4, 5 }).IsStrictlyMonotonic());
+            Assert.IsTrue((new double[] { -1.5, 0, 0.5, 3, 4, 4.2, 5 }).IsStrictlyMonotonic());
+            Assert.IsTrue((new double[] { 5, 4.2, 4, 3, 0.5, 0, -1.5 }).IsStrictlyMonotonic());
+            Assert.IsFalse((new double[] { 5, 4, 4, 3, 0.5, 0, -1.5 }).IsStrictlyMonotonic());
+        }
+    }
+}
---
+++ Utils.Mathnet/Sequence.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace Net.Teirlinck.Utils.Mathnet
+{
+    public static class Sequence
+    {
+        public static bool IsMonotonic(this double[] sequence)
+        {
+            if (sequence == null || sequence.Length < 2)
+                return false;
+
+            int sign = 0;
+            int counter = 0;
+
+            while (sign == 0 && counter < sequence.Length - 1)
+                sign = Math.Sign(sequence[counter + 1] - sequence[counter++]);
+
+            if (sign == 0)
+                return true; // That means that all items in the sequence are equal to 0
+
+            for (int i = counter; i < sequence.Length - 1; i++)
+            {
+                int localSign = Math.Sign(sequence[i + 1] - sequence[i]);
+
+                if (localSign != sign && localSign != 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsMonotonic(this int[] sequence)
+        {
+            if (sequence == null || sequence.Length == 0)
+                return false;
+
+            return sequence.Select(i => (double)i).ToArray().IsMonotonic();
+        }
+
+        public static bool IsStrictlyMonotonic(this double[] sequence)
+        {
+            if (sequence == null || sequence.Length < 2)
+                return false;
+
+            int sign = Math.Sign(sequence[1] - sequence[0]);
+
+            if (sign == 0)
+                return false;
+
+            for (int i = 1; i < sequence.Length - 1; i++)
+            {
+                int localSign = Math.Sign(sequence[i + 1] - sequence[i]);
+
+                if (localSign != sign)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsStrictlyMonotonic(this int[] sequence)
+        {
+            if (sequence == null || sequence.Length == 0)
+                return false;
+
+            return sequence.Select(i => (double)i).ToArray().IsStrictlyMonotonic();
+        }
+
+    }
+}
